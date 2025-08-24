0b35886b6be11313593d9fc6be1ddb08c79400d7
DevHannan-Official <dev.hannan.official@gmail.com>
2025-08-24 12:41:43 +0500

    Added Middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet);
 
 app.get("/", (req, res) => {
   res.send("Hello From Stacker Server");
