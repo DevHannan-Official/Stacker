c50a50642c7176c0d09ebbcdf3d5810e040323ba
DevHannan-Official <dev.hannan.official@gmail.com>
2025-08-24 13:14:50 +0500

    Added Routes

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import {
-  asyncHandler,
-  ErrorMiddleware,
-} from "./middlewares/error.middleware.js";
-import ErrorHandler from "./lib/error-handler.js";
+import { ErrorMiddleware } from "./middlewares/error.middleware.js";
+
+import authRoutes from "./routes/auth.route.js";
+import chatsRoutes from "./routes/chats.route.js";
+import messagesRoutes from "./routes/message.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import profileRoutes from "./routes/profile.route.js";
 
 const app = express();
 
@@ -19,12 +21,16 @@ app.use(cookieParser());
 app.use(helmet());
 
 // Routes
-app.get(
-  "/",
-  asyncHandler(async (req, res, next) => {
-    next(new ErrorHandler("Debug Error", 400));
-  })
-);
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to **Stacker API**" });
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/chats", chatsRoutes);
+app.use("/api/messages", messagesRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/profile", profileRoutes);
 
 // Custom Middlewares
 app.use(ErrorMiddleware);
