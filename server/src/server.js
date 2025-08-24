0f566b2a06f81db70185a13e2fe52b2ad0fabd8c
DevHannan-Official <dev.hannan.official@gmail.com>
2025-08-24 14:40:29 +0500

    Completed Server Configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import { dbConnect } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import processErrorHandlers from "./lib/process-errors.js";
 
 const PORT = ENV.PORT;
 
@@ -8,3 +9,5 @@ app.listen(PORT, async () => {
   console.log("✔ Stacker HTTP Server is running on port", PORT);
   await dbConnect();
 });
+
+processErrorHandlers();
