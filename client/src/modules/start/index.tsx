db468a253d018122d762b6dc2806616082a3bf33
DevHannan-Official <dev.hannan.official@gmail.com>
2025-08-29 17:15:43 +0500

    fix link

--- client/src/modules/start/index.tsx
+++ client/src/modules/start/index.tsx
@@ -62,10 +62,7 @@ const StartPage = () => {
               <p className="text-red-500 font-medium text-sm">{error}</p>
             )}
           </div>
-          <Link
-            className="link self-end"
-            href="/reset-password?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4YjEwOWU4MjRjNzZhNGI4NmY4YWIyYSIsImlhdCI6MTc1NjQ2OTM0MywiZXhwIjoxNzU2NDcxMTQzfQ.rQfDKcH0sNhEW5i8qImJbqisxA4p5a-RyfwNRmAiuyM"
-          >
+          <Link className="link self-end" href="/forget-password">
             Forget Password?
           </Link>
           <Button variant={"primary"} additionalClasses="w-full" type="submit">
